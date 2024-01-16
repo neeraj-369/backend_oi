c21a2e4efc2985e5e7d67ce09d40cdf734e29a5b
neeraj-369 <codewriter369@gmail.com>
2024-01-16 15:53:07 +0530

    cors

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // app.use('/app', AppRoutes);
 // app.use('/version', versionRoutes);
 
